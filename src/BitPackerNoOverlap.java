ee19eb17393f1cbe4b7f6cd61243929b77cf80e0
Sofiane949 <sofianekhourta@gmail.com>
2025-11-02 15:49:55 +0100

    implémentation méthodes get et compress NoOverlap + tests

--- src/BitPackerNoOverlap.java
+++ src/BitPackerNoOverlap.java
@@ -1,16 +1,72 @@
 public class BitPackerNoOverlap implements BitPacker {
-    @java.lang.Override
-    public void compress(int[] input) {
+    private int k = 0;
+    private int nPerInt = 0;
+    private int originalLength = 0;
+    private int[] compressed;
 
+    public BitPackerNoOverlap() {
+    }
+    @Override
+    public void compress(int[] input) {
+        originalLength = input.length;
+        maxK(input);
+
+        if (k == 32) { // Cas où les nombres sont trop grands ou négatifs
+            compressed = input.clone(); // Pas de compression possible
+            nPerInt = 1;
+            return;
+        }
+        if (k == 0) { // Cas d'un tableau vide
+            compressed = new int[0];
+            return;
+        }
+
+        nPerInt = 32 / k;
+        int compressedLength = (int) Math.ceil((double) input.length / nPerInt);
+        compressed = new int[compressedLength];
+        int i = 0;
+        int j = 0;
+        for (int a : input) {
+            if (j + k > 32) {
+                i++;
+                j = 0;
+            }
+
+            compressed[i] = compressed[i] | (a << j);
+            j += k;
+        }
     }
 
-    @java.lang.Override
+    @Override
     public void decompress(int[] output) {
 
     }
 
-    @java.lang.Override
+    @Override
     public int get(int i) {
-        return 0;
+        int index = i / nPerInt;
+        int posBits = (i % nPerInt) * k;
+        int r = compressed[index] >> posBits;
+        int mask = (1 << k) - 1;
+        r = r & mask;
+        return r;
+    }
+
+    public void maxK(int[] tab) {
+        k = 0;
+        for (int a : tab) {
+            int len = Integer.toBinaryString(a).length();
+            if (len > k) {
+                k = len;
+            }
+        }
+    }
+
+    public int[] getCompressed() {
+        return compressed;
+    }
+
+    public int getK() {
+        return k;
     }
 }
--- test/BitPackerNoOverlapTest.java
+++ test/BitPackerNoOverlapTest.java
@@ -1,9 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BitPackerNoOverlapTest {
 
     @org.junit.jupiter.api.Test
     void compress() {
+        int[] a = {0,1,2};
+        BitPackerNoOverlap bp = new BitPackerNoOverlap();
+        bp.compress(a);
+        assertEquals (2, bp.getK());
+        assertEquals(1, bp.getCompressed().length);
+        int[] b = {15,14,255,21,1};
+        BitPackerNoOverlap bp1 = new BitPackerNoOverlap();
+        bp.compress(b);
+        assertEquals (8, bp.getK());
+        assertEquals (2, bp.getCompressed().length);
+        assertEquals("10101111111110000111000001111", Integer.toBinaryString(bp.getCompressed()[0]));
     }
 
     @org.junit.jupiter.api.Test
@@ -12,5 +27,26 @@ class BitPackerNoOverlapTest {
 
     @org.junit.jupiter.api.Test
     void get() {
+        int[] a = {15,14,255,21};
+        BitPackerNoOverlap bp = new BitPackerNoOverlap();
+        bp.compress(a);
+        assert (bp.get(0) == 15 && bp.get(1) == 14 && bp.get(2) == 255 && bp.get(3) == 21);
+    }
+
+    @Test
+    void maxK() {
+        int[] a = {0,1,2};
+        int[] b = {0,1,2,8,1023};
+        int[] d = {0,1,2,8,1024};
+        int[] c = {0,1,2,8,1023,2047};
+        BitPackerNoOverlap bp = new BitPackerNoOverlap();
+        bp.maxK(a);
+        assert(bp.getK() == 2);
+        bp.maxK(b);
+        assert(bp.getK() == 10);
+        bp.maxK(c);
+        assert(bp.getK() == 11);
+        bp.maxK(d);
+        assert(bp.getK() == 11);
     }
 }
